3d034438e2ed4f6d7674877844b68b9f28633495
Diego Ochoa <diegoa2000alt@gmail.com>
2019-06-12 20:25:53 -0500

    Clean code for drone and drone generator.

--- enemies/drone_generator/DroneGenerator.gd
+++ enemies/drone_generator/DroneGenerator.gd
@@ -1,42 +1,14 @@
 extends NPC
 
-export var debug : bool = true
-
-var states_stack = []
-var current_state = null
-
-onready var states_map = {
-	"idle" : $States/Idle,
-	"send_drone" : $States/SendDrone
-}
-
 func _ready() -> void:
 	._ready()
+	states_map = {
+		"idle" : $States/Idle,
+		"send_drone" : $States/SendDrone
+	}
 	states_stack.push_back($States/Idle)
 	current_state = states_stack[0]
 	_change_state("idle")
-	
-func _process(delta: float) -> void:
-		
-	evaluate_health()
-	var state_name = current_state.update(self, delta)	
-	if state_name and not state_name.empty():
-		_change_state(state_name)
-	
-func _change_state(_state_name : String) -> void:
-	current_state.exit(self)
-	
-	if _state_name == "previous":
-		states_stack.pop_front()
-	else:
-		var new_state = states_map[_state_name]
-		states_stack[0] = new_state
-	
-	current_state = states_stack[0]
-	if _state_name != "previous":
-		current_state.enter(self)
-	DebugManager.debug("turret-current_state", current_state.name, debug)
-	DebugManager.debug_states("turret-states", states_stack, debug)
 
 func _on_DroneGenerator_area_entered(area: Area2D) -> void:
 	process_area_entered(area)
