f8719b864bf73253264b830f7a05cbbd9730e4cc
Diego Ochoa <diegoa2000alt@gmail.com>
2019-06-12 20:26:02 -0500

    Clean code for turret

--- enemies/turret/Turret.gd
+++ enemies/turret/Turret.gd
@@ -1,56 +1,22 @@
 extends NPC
 
-export var debug : bool = false
-
-var states_stack = []
-var current_state = null
-
-onready var explosion : PackedScene = preload("res://particles/BulletExplosion.tscn")
-
-onready var states_map = {
-	"idle" : $States/Idle,
-	"shoot_bullets" : $States/ShootBullet,
-	"shoot_missile" : $States/ShootMissile
-}
-
 func _ready() -> void:
 	._ready()
+	states_map = {
+		"idle" : $States/Idle,
+		"shoot_bullets" : $States/ShootBullet,
+		"shoot_missile" : $States/ShootMissile
+	}
 	states_stack.push_back($States/Idle)
 	current_state = states_stack[0]
 	_change_state("idle")
 	
 func _process(delta: float) -> void:
 	
-	DebugManager.debug("turret-health", health, debug)
-	evaluate_health()
-
-	call_deferred("aim_at_player", delta)
+	aim_at_player(delta)
 	
-	var state_name = current_state.update(self, delta)	
-	if state_name and not state_name.empty():
-		_change_state(state_name)
+	._process(delta)
 		
-func _physics_process(delta: float) -> void:
-	
-	var state_name = current_state.fixed_update(self, delta)
-	if state_name and not state_name.empty():
-		_change_state(state_name)
-		
-func _change_state(_state_name : String) -> void:
-	current_state.exit(self)
-	
-	if _state_name == "previous":
-		states_stack.pop_front()
-	else:
-		var new_state = states_map[_state_name]
-		states_stack[0] = new_state
-	
-	current_state = states_stack[0]
-	if _state_name != "previous":
-		current_state.enter(self)
-	DebugManager.debug("turret-current_state", current_state.name, debug)
-	DebugManager.debug_states("turret-states", states_stack, debug)
-
 func _on_Turret_area_entered(area: Area2D) -> void:
 	process_area_entered(area)
 
