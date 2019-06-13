fa6a1c05cf802b5afd8aea5faf0bc25e5ccf31d4
Diego Ochoa <diegoa2000alt@gmail.com>
2019-06-12 20:26:46 -0500

    Add instant kill constants

--- autoload/weapon_manager.gd
+++ autoload/weapon_manager.gd
@@ -11,6 +11,8 @@ const PLAYER_SUPER_BULLET_DAMAGE_BASE : float = 8.0
 const ENEMY_BULLETS_DAMAGE_BASE : float = 20.0
 const ENEMY_MISSILE_DAMAGE_BASE : float = 50.0
 
+const DAMAGE_INSTA_KILL : float = 99999.0
+
 func _ready():
 	if get_tree().get_root().has_node(ConstManager.GAME_NODE_NAME):
 		weapon_container = get_tree().get_root().get_node(ConstManager.GAME_NODE_NAME).get_node(ConstManager.WEPONS_NODE_NAME)
@@ -19,3 +21,5 @@ func add_weapon(_weapon, _scale_x=1, _scale_y=1) -> void:
 	if weapon_container:
 		_weapon.scale = Vector2(_scale_x, _scale_y)
 		weapon_container.add_child(_weapon)
+	else:
+		print("no weapon container")
